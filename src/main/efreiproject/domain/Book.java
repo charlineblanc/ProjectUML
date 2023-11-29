feb0c7243ab76f76e49ecdc11e733872c93de4b0
audie17 <audrey.damiba@efrei.net>
2023-11-29 16:00:33 +0200

    Book update

--- src/main/efreiproject/domain/Book.java
+++ src/main/efreiproject/domain/Book.java
@@ -1,21 +1,27 @@
-package efreiproject.domain;
 
+package efreiproject.domain;
 public class Book {
     private String isbn;
     private String title;
     private String author;
     private float price;
+    private String genre;
+    private int quantityAvailable;
 
     private Book() {}
-
 
     public Book(Builder builder) {
         this.isbn = builder.isbn;
         this.title = builder.title;
         this.author = builder.author;
         this.price = builder.price;
+        this.genre = builder.genre;
+        this.quantityAvailable = builder.quantityAvailable;
     }
 
+    public String getIsbn() {
+        return isbn;
+    }
 
     public String getTitle() {
         return title;
@@ -29,8 +35,12 @@ public class Book {
         return price;
     }
 
-    public String getIsbn() {
-        return isbn;
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getQuantityAvailable() {
+        return quantityAvailable;
     }
 
     @Override
@@ -40,15 +50,20 @@ public class Book {
                 ", title='" + title + '\'' +
                 ", author='" + author + '\'' +
                 ", price=" + price +
+                ", genre='" + genre + '\'' +
+                ", quantityAvailable=" + quantityAvailable +
                 '}';
     }
 
     public static class Builder {
+        // Mandatory fields
         private String isbn;
         private String title;
         private String author;
-        private float price;
-
+        // Optional fields with default values
+        private float price = 0.0f;
+        private String genre = "";
+        private int quantityAvailable = 0;
 
         public Builder setIsbn(String isbn) {
             this.isbn = isbn;
@@ -69,6 +84,17 @@ public class Book {
             this.price = price;
             return this;
         }
+
+        public Builder setGenre(String genre) {
+            this.genre = genre;
+            return this;
+        }
+
+        public Builder setQuantityAvailable(int quantityAvailable) {
+            this.quantityAvailable = quantityAvailable;
+            return this;
+        }
+
         public Book build() {
             return new Book(this);
         }
